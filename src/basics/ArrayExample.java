5f87ebbd968ae82a78165d9f24d026c10521785a
prosperityadedayo <prosperity.adedayo@binghamuni.edu.ng>
2025-06-17 13:52:23 +0100

    Repushed missing accountant file

--- src/basics/ArrayExample.java
+++ src/basics/ArrayExample.java
@@ -5,10 +5,12 @@ public class ArrayExample {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int[] myArray = {};
+        int[] myArray = {1,2,3,4,5};
         int[] anotherArray = new int[10];
         String[] gender = {"Female", "Male"};
 
+        myArray[1] = 13;
+        anotherArray[5] = 17;
 
         for(int i = 0; i < anotherArray.length; i++) {
             System.out.println("Insert Item at: " +i);
